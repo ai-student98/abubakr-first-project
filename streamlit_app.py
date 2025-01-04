2ea47530728b622a9f2e722ad2755ee856e29fc1
ai-student98 <alavimeh493@gmail.com>
2025-01-04 11:55:27 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
-st.title('😁 Abubakr first project')
+st.title('😁 Abubakr First APP')
 
-st.write('Построим свою первую модель машинного обучения!')
+st.info('This is app builds a machine learning model!')
+
+with st.expander('Data'):
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')  
+  st.write('**X**')
+  X_raw = df.drop('species', axis=1)
+  X_raw
+
+  st.write('**y**')
+  y_raw = df.species
+  y_raw
